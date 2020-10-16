4cb4fabd606d3a7bc4400e51bed0d76c186f01d0
Prior <471291315@qq.com>
2020-10-16 20:45:42 +0800

    dataset.py & transforms.py

---
+++ dataset.py
@@ -0,0 +1,89 @@
+import torch.utils.data as data
+
+from PIL import Image
+import os
+import os.path
+import numpy as np
+from numpy.random import randint
+
+
+class VideoRecord(object):
+    def __init__(self, row):
+        self._data = row
+
+    @property
+    def path(self):
+        return self._data[0]
+
+    @property
+    def num_frames(self):
+        return int(self._data[1])
+
+    @property
+    def label(self):
+        return int(self._data[2])
+
+
+class I3DDataSet(data.Dataset):
+    def __init__(self, root_path, list_file, clip_length=64,
+                 modality='RGB', image_tmpl='img_{:05d}.jpg',
+                 transform=None, random_shift=True, test_mode=False):
+        self.root_path = root_path
+        self.list_file = list_file
+        self.clip_length = clip_length
+        self.modality = modality
+        self.image_tmpl = image_tmpl
+        self.transform = transform
+        self.random_shift = random_shift
+        self.test_mode = test_mode
+
+        self._parse_list()
+
+    def _load_image(self, directory, idx):
+        root_path = os.path.join(self.root_path, 'rawframes/')  # ../data/ucf101/rawframes/
+        directory = os.path.join(root_path, directory)
+
+        if self.modality == 'RGB' or self.modality == 'RGBDiff':
+            return [Image.open(os.path.join(directory, self.image_tmpl.format(idx))).convert('RGB')]
+        elif self.modality == 'Flow':
+            x_img = Image.open(os.path.join(directory, self.image_tmpl.format('x', idx))).convert('L')
+            y_img = Image.open(os.path.join(directory, self.image_tmpl.format('y', idx))).convert('L')
+
+            return [x_img, y_img]
+
+    def _parse_list(self):
+        self.video_list = [VideoRecord(x.strip().split(' ')) for x in
+                           open(os.path.join(self.root_path, self.list_file))]
+
+    def _sample_index(self, record):
+        tick = record.num_frames - self.clip_length
+        if tick < 0:
+            index = 0
+        else:
+            if not self.test_mode:
+                index = randint(tick + 1) if self.random_shift else int(tick / 2.0)
+            else:
+                index = int(tick / 2.0)
+        return index
+
+    def __getitem__(self, index):
+        record = self.video_list[index]
+        idx = self._sample_index(record)
+        return self.get(record, idx)
+
+    def get(self, record, index):
+        images = list()
+        p, seg_imgs = int(index), None
+        for i in range(self.clip_length):
+            if p < record.num_frames:
+                seg_imgs = self._load_image(record.path, p)
+                images.extend(seg_imgs)
+                p += 1
+            else:
+                images.extend(seg_imgs)
+
+        process_data = self.transform(images)
+        return process_data, record.label
+
+    def __len__(self):
+        return len(self.video_list)
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import torch
-from models.backbones import resnet3d
-from models import *
+from torch.nn.utils import clip_grad_norm
 
-import torch.nn as nn
-from torchvision.models.resnet import *
+from models.backbones import resnet3d
+from models import i3d
+from dataset import I3DDataSet
+from transforms import *
+from opts import parser
 
 
 def test_resnet3d():
@@ -15,16 +17,36 @@ def test_resnet3d():
 
 
 def test_i3d():
-    model = i3d_resnet50(pretrained2d=True, num_classes=101)
+    model = getattr(i3d, 'i3d_resnet50')(pretrained2d=True, num_classes=101)
     # batch_size x channels x frames x height x width
     dummy_input = torch.rand(8, 3, 10, 224, 224)
     output = model(dummy_input)
     print(output.shape)
 
 
-def main():
+def test():
     test_i3d()
 
 
+best_prec1 = 0
+
+
+def main():
+    global args, best_prec1
+    args = parser.parse_args()
+
+    if args.dataset == 'ucf101':
+        num_class = 101
+    elif args.dataset == 'hmdb51':
+        num_class = 51
+    elif args.dataset == 'kinetics':
+        num_class = 400
+    else:
+        raise ValueError('Unknown dataset ' + args.dataset)
+
+    model = getattr(i3d, args.arch)(pretrained2d=True, num_class=num_class,
+                                    dropout_ratio=args.dropout)
+
+
 if __name__ == '__main__':
-    main()
+    test()
